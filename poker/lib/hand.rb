e25fe143af78b8b580b0de48869b0152bc47896d
App Academy Student <student@appacademy.io>
2015-11-04 17:30:01 -0800

    complete hand class

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -15,8 +15,8 @@ HAND_VALUES = [
 class Hand
 attr_accessor :hand, :hand_value
 
-  def initialize()
-    @hand = []
+  def initialize(cards = [])
+    @hand = cards
     @hand_value = nil
   end
 
@@ -52,6 +52,22 @@ attr_accessor :hand, :hand_value
     end
   end
 
+  def select
+    @hand.select do |card|
+      yield(card)
+    end
+  end
+
+  def reject
+    @hand.reject do |card|
+      yield(card)
+    end
+  end
+
+  def size
+    @hand.size
+  end
+  
   def sort!
     @hand.sort! {|card1, card2 | card1.value <=> card2.value}
   end
@@ -59,50 +75,117 @@ attr_accessor :hand, :hand_value
   def find_hand
     if straight_flush?
       @hand_value = :straight_flush
-      break
     elsif four_of_a_kind?
       @hand_value = :four_of_a_kind
-      break
     elsif full_house?
       @hand_value = :full_house
-      break
     elsif flush?
       @hand_value = :flush
-      break
     elsif straight?
       @hand_value = :straight
-      break
     elsif three_of_a_kind?
       @hand_value = :three_of_a_kind
-      break
     elsif two_pair?
       @hand_value = :two_pair
-      break
     elsif one_pair?
       @hand_value = :one_pair
-      break
     else
       @hand_value = :high_card
     end
+
+    @hand_value
   end
 
   def compare_hands(opp_hand)
-    my_value = HAND_VALUES.find_index(hand_value)
-    opp_value = HAND_VALUES.find_index(opp_hand.hand_value)
+    my_value = HAND_VALUES.find_index(self.find_hand)
+    opp_value = HAND_VALUES.find_index(opp_hand.find_hand)
 
     case my_value <=> opp_value
     when -1
-      puts "opp wins"
+      "opp wins"
     when 0
       break_tie(opp_hand)
     when 1
-      puts "you win"
+      "you win"
     end
   end
 
+  def compare_cards(card1, card2)
+    card1.value <=> card2.value
+  end
+
+  def compare_values(card1_value, card2_value)
+    card1_value <=> card2_value
+  end
+
   def break_tie(opp_hand)
-    puts "in break_tie"
-    puts "you win"
+    winner = 0
+    case self.hand_value
+    when :high_card
+      winner = break_high_card_tie(self, opp_hand)
+    when :straight_flush
+      winner = break_high_card_tie(self, opp_hand)
+    when :one_pair
+      winner = break_n_tie(opp_hand, 2)
+      winner = break_leftover_tie(opp_hand, 2) if winner.zero?
+    when :two_pair
+      winner = break_n_tie(opp_hand, 2)
+      winner = break_leftover_tie(opp_hand, 2) if winner.zero?
+    when :three_of_a_kind,
+      winner = break_n_tie(opp_hand, 3)
+      winner = break_leftover_tie(opp_hand, 3) if winner.zero?
+    when :straight
+      winner = break_high_card_tie(self, opp_hand)
+    when :flush
+      winner = break_high_card_tie(self, opp_hand)
+    when :full_house
+      winner = break_n_tie(opp_hand, 3)
+      if winner == 0
+        winner = break_n_tie(opp_hand, 2)
+      end
+    when :four_of_a_kind
+      winner = break_n_tie(opp_hand, 4)
+      winner = break_leftover_tie(opp_hand, 4) if winner.zero?
+    end
+
+    case winner
+    when -1
+      "opp wins"
+    when 0
+      "you tie"
+    when 1
+      "you win"
+    end
+  end
+
+  def break_n_tie(opp_hand, n) # break n-of-a-kind ties
+    pair1 = self.count_hand.select { |val, count| count == n }.keys
+    pair2 = opp_hand.count_hand.select { |val, count| count == n }.keys
+    winner = compare_values(pair1.last[0], pair2.last[0])
+
+    if winner == 0 && pair1.size == 2 # only for two pairs
+      winner = compare_values(pair1.first[0], pair2.first[0])
+    end
+
+    winner
+  end
+
+  def break_leftover_tie(opp_hand, n)
+    upd_my_hand = self.count_hand.reject { |val, count| count == n }.keys
+    upd_opp_hand = opp_hand.count_hand.reject { |val, count| count == n }.keys
+    break_high_card_tie(upd_my_hand, upd_opp_hand, true)
+  end
+
+  def break_high_card_tie(my_hand, opp_hand, value=false)
+    winner = 0
+    (1..opp_hand.size).each do |i|
+      winner = value ?
+        compare_values(my_hand[-i], opp_hand[-i]) :
+        compare_cards(my_hand[-i], opp_hand[-i])
+      break unless winner == 0
+    end
+
+    winner
   end
 
   def count_hand
@@ -110,7 +193,6 @@ attr_accessor :hand, :hand_value
     self.each do |card|
       count[card.value] += 1
     end
-
     count
   end
 
@@ -158,7 +240,4 @@ attr_accessor :hand, :hand_value
     count = count_hand
     count.values.include?(2)
   end
-
-
-
 end
--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -2,9 +2,57 @@ require 'hand'
 
 describe 'Hand' do
 
-  let(:hand) { Hand.new }
+  let(:hand) {Hand.new}
   let(:test_card) {Card.new(4, :spade)}
   let(:suits) {[:spade, :heart, :club, :diamond]}
+
+  let(:straight_flush_hand) { Hand.new([
+    Card.new(2, :spade),
+    Card.new(3, :spade),
+    Card.new(4, :spade),
+    Card.new(5, :spade),
+    Card.new(6, :spade)
+    ])}
+
+  let(:full_house_hand) { Hand.new([
+    Card.new(13, :spade),
+    Card.new(13, :spade),
+    Card.new(13, :heart),
+    Card.new(6, :club),
+    Card.new(6, :spade)
+    ])}
+
+  let(:two_pair_hand) { Hand.new([
+    Card.new(2, :spade),
+    Card.new(2, :spade),
+    Card.new(10, :heart),
+    Card.new(6, :club),
+    Card.new(6, :spade)
+    ])}
+
+  let(:straight_flush_hand2) { Hand.new([
+    Card.new(2, :heart),
+    Card.new(3, :heart),
+    Card.new(4, :heart),
+    Card.new(5, :heart),
+    Card.new(6, :heart)
+    ])}
+
+  let(:full_house_hand2) { Hand.new([
+    Card.new(2, :spade),
+    Card.new(2, :spade),
+    Card.new(2, :heart),
+    Card.new(6, :club),
+    Card.new(6, :spade)
+    ])}
+
+  let(:two_pair_hand2) { Hand.new([
+    Card.new(2, :spade),
+    Card.new(2, :spade),
+    Card.new(13, :heart),
+    Card.new(6, :club),
+    Card.new(6, :spade)
+    ])}
 
   it "initializes with empty hand" do
     expect(hand.hand).to be_empty
@@ -28,6 +76,15 @@ describe 'Hand' do
     expect(hand.discard(4)).to eq(hand_checker)
   end
 
+  it "doesn't discard if your hand is empty" do
+    expect(hand.discard(0)).to eq(hand)
+  end
+
+  it "interpolates Hand to string" do
+    5.times {|i| hand.add(Card.new(i+2, :spade))}
+    expect(hand.to_s).to eq("2 ♠, 3 ♠, 4 ♠, 5 ♠, 6 ♠")
+  end
+
   it "sorts a hand properly" do
     5.times {|i| hand.add(Card.new(i+2, :spade))}
     hand_checker = Hand.new
@@ -36,43 +93,58 @@ describe 'Hand' do
   end
 
   it "recognizes a straight flush" do
-    5.times {|i| hand.add(Card.new(i+2, :spade))}
-    expect(hand.straight_flush?).to be(true)
+    expect(straight_flush_hand.straight_flush?).to be(true)
   end
 
   it "recognizes a straight" do
-    4.times {|i| hand.add(Card.new(i+2, :spade))}
-    hand.add(Card.new(6, :heart))
-    expect(hand.straight?).to be(true)
+    expect(straight_flush_hand.straight?).to be(true)
   end
 
   it "recognizes a flush" do
-    5.times {|i| hand.add(Card.new(i+4, :spade))}
-    expect(hand.flush?).to be(true)
+    expect(straight_flush_hand.flush?).to be(true)
   end
 
   it "recognizes a full house" do
-    3.times {|i| hand.add(Card.new(7, suits[i]))}
-    2.times {|i| hand.add(Card.new(6, suits[i]))}
-    expect(hand.full_house?).to be(true)
+    expect(full_house_hand.full_house?).to be(true)
   end
 
   it "recognizes a three of a kind" do
-    3.times {|i| hand.add(Card.new(7, suits[i]))}
-    2.times {|i| hand.add(Card.new(i + 4, suits[i]))}
-    expect(hand.three_of_a_kind?).to be(true)
+    expect(full_house_hand.three_of_a_kind?).to be(true)
   end
 
   it "recognizes two pair" do
-    2.times {|i| hand.add(Card.new(7, suits[i]))}
-    2.times {|i| hand.add(Card.new(i + 4, suits[i]))}
-    hand.add(Card.new(4, suits[3]))
-    expect(hand.two_pair?).to be(true)
+    expect(two_pair_hand.two_pair?).to be(true)
   end
 
   it "recognizes one pair" do
-    2.times {|i| hand.add(Card.new(2, suits[i]))}
-    3.times {|i| hand.add(Card.new(i + 4, suits[i]))}
-    expect(hand.one_pair?).to be(true)
+    expect(full_house_hand.one_pair?).to be(true)
+  end
+
+  it "sets hand value corrently" do
+    3.times {|i| hand.add(Card.new(7, suits[i]))}
+    2.times {|i| hand.add(Card.new(6, suits[i]))}
+    expect(hand.find_hand).to be(:full_house)
+  end
+
+  describe "#compares hands" do
+    it "... when you have winning hand" do
+      expect(straight_flush_hand.compare_hands(two_pair_hand)).to eq("you win")
+    end
+
+    it "... when you have losing hand" do
+      expect(two_pair_hand.compare_hands(full_house_hand)).to eq("opp wins")
+    end
+
+    it "... breaks ties when you have winning hand" do
+      expect(full_house_hand.compare_hands(full_house_hand2)).to eq("you win")
+    end
+
+    it "... breaks ties when you have losing hand" do
+      expect(two_pair_hand.compare_hands(two_pair_hand2)).to eq("opp wins")
+    end
+
+    it "... returns ties when you have tie" do
+      expect(straight_flush_hand.compare_hands(straight_flush_hand2)).to eq ('you tie')
+    end
   end
 end
